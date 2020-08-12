f5de2d527593886ff570808673ed4e989484126b
bharath210 <barathkumargoudpalsa@gmail.com>
2020-08-12 20:39:00 +0530

    committing

--- SocketDemo/ServerSider.py
+++ SocketDemo/ServerSider.py
@@ -1,3 +1,13 @@
 import socket
 
-s = socket,socket()
+s = socket.socket()
+print('Socket is Created')
+s.bind(('localhost',9999))
+s.listen(3)
+
+while True:
+    c,add = s.accept()
+    name = c.recv(1024).decode()
+    print('connected with',add,name)
+    c.send(bytes('you are connected to Vampie server','utf-8'))
+    c.close()
